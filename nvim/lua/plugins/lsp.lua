2df1a0302e6f0442953c7edf307a379023d01c0c
Wei Wang <ww000721@gmail.com>
2025-05-06 00:39:14 -0400

    Update lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,20 @@
 return {
     "neovim/nvim-lspconfig",
-    version = "0.1.7",  -- latest version that works with Neovim 0.9.x
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    version = "0.1.7", -- latest version works with Neovim 0.9.x
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        {
+            "williamboman/mason.nvim",
+            build = ":MasonUpdate",
+            config = true, -- equivalent to require("mason").setup()
+        },
+        {
+          "williamboman/mason-lspconfig.nvim",
+        },
+        {
+          "hrsh7th/cmp-nvim-lsp",
+        },
+    },
     config = function()
         require("mason-lspconfig").setup({
             ensure_installed = { "pyright", "clangd", "bashls" },
@@ -10,7 +23,7 @@ return {
 
         local lspconfig = require("lspconfig")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+        
         -- Setup handlers for installed servers
         require("mason-lspconfig").setup_handlers({
             function(server)
