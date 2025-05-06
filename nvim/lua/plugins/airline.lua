670b660d60da716fc0bc923291c15e7489bdb3ce
Wei Wang <ww000721@gmail.com>
2025-05-06 00:43:45 -0400

    Update airline.lua

--- nvim/lua/plugins/airline.lua
+++ nvim/lua/plugins/airline.lua
@@ -1,30 +1,26 @@
 return {
-    {
-        "vim-airline/vim-airline",
-        lazy = false,
-        init = function()
-            -- Airline UI config
-            vim.g.airline_theme = "molokai"
-            vim.g.airline_powerline_fonts = 1
+    "vim-airline/vim-airline",
+    lazy = false,
+    init = function()
+        -- Airline UI config
+        vim.g.airline_theme = "molokai"
+        vim.g.airline_powerline_fonts = 1
 
-            -- Enable tabline
-            vim.g["airline#extensions#tabline#enabled"] = 1
-            vim.g["airline#extensions#tabline#formatter"] = "unique_tail"
+        -- Enable tabline
+        vim.g["airline#extensions#tabline#enabled"] = 1
+        vim.g["airline#extensions#tabline#formatter"] = "unique_tail"
 
-            -- Enable branch and hunk info (requires fugitive + gitgutter)
-            vim.g["airline#extensions#branch#enabled"] = 1
-            vim.g["airline#extensions#hunks#enabled"] = 1
+        -- Enable branch and hunk info (requires fugitive + gitgutter)
+        vim.g["airline#extensions#branch#enabled"] = 1
+        vim.g["airline#extensions#hunks#enabled"] = 1
 
-            -- Optional: clear error section
-            vim.g["airline_section_error"] = ""
-        end,
-        dependencies = {
-            "tpope/vim-fugitive",     -- Git branch info
-            "airblade/vim-gitgutter", -- Git diff info (added/changed/removed lines)
-        },
-    },
-    {
-        "vim-airline/vim-airline-themes",
-        lazy = false,
+        -- Optional: clear error section
+        vim.g["airline_section_error"] = ""
+    end,
+    dependencies = {
+        "vim-airline/vim-airline-themes",   -- Airline themes
+
+        "tpope/vim-fugitive",     -- Git branch info
+        "airblade/vim-gitgutter", -- Git diff info (added/changed/removed lines)
     },
 }
