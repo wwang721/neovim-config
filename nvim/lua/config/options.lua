9c89efd0195d5e57d8a4d22bbd0df39f637ddbcd
Wei Wang <ww000721@gmail.com>
2025-05-07 15:12:07 -0400

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,26 +3,47 @@ vim.g.deprecation_warnings = false
 
 local opt = vim.opt
 
--- already shown in the statusline
-opt.showmode = false -- hide mode messages in the left-bottom corner
-opt.showcmd = false  -- hide partial key sequences in the bottom-right corner
+opt.showmode = false -- hide mode messages in the left-bottom corner since we have a statusline
+opt.showcmd = true  -- show partial key sequences in the bottom-right corner
 
+opt.ruler = false -- Disable the default ruler
 opt.laststatus = 3 -- global statusline
 opt.mouse = "a" -- Enable mouse mode
+
+opt.scrolloff = 4 -- Lines of context
+opt.sidescrolloff = 8 -- Columns of context
 
 opt.number = true -- Print line number
 opt.cursorline = true -- Enable highlighting of the current line
 -- opt.termguicolors = true -- True color support
 
-opt.guicursor = "" -- disable cursor shape changes
+-- opt.guicursor = "" -- disable cursor shape changes
+opt.guicursor = {
+  "n-v-sm:block",      -- block cursor in normal, visual, and select mode
+  "i-c-ci-ve:ver25",       -- vertical bar (25%) in insert, command-line, and related modes
+  "r-cr-o:hor20"         -- horizontal bar (20%) in replace and operator-pending modes
+}
 
 opt.tabstop = 4 -- Number of spaces tabs count for
 opt.softtabstop = 4 -- Number of spaces that a <Tab> counts for while editing
+opt.shiftround = true -- Round indent
 opt.shiftwidth = 4 -- Size of an indent
 opt.expandtab = true -- Use spaces instead of tabs
 
 opt.wrap = false -- Disable line wrap
 opt.linebreak = true -- Wrap lines at convenient points, only applies when wrap is enabled.
+
+opt.fillchars:append({ eob = ' ' }) -- Replace ~ on empty lines after the end of buffer with spaces
+
+opt.foldlevel = 99 -- ensures folds are open by default
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true
+  opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+  opt.foldmethod = "expr"
+else
+  opt.foldmethod = "indent"
+end
+
 
 -- Turn off diagnostics completely (I don't want to see any errors)
 vim.diagnostic.config({ virtual_text = false, signs = false, underline = false })
@@ -30,8 +51,4 @@ vim.diagnostic.config({ virtual_text = false, signs = false, underline = false }
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
 
--- Replace ~ on empty lines after the end of buffer with spaces
-vim.opt.fillchars:append({ eob = ' ' })
-
 vim.cmd("colorscheme molokai")
-
