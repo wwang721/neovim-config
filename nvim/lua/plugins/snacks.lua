0d4ebb4c2390be7f998f4d7349c271538bcfebfb
Wei Wang <ww000721@gmail.com>
2025-05-09 13:19:34 -0400

    Update snacks.lua

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -25,7 +25,7 @@ return {
                 pick = function(cmd, opts)
                     return LazyVim.pick(cmd, opts)()
                 end,
-                header = [[
+                header = string.format([[
   ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
   ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
   ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
