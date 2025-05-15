54589ee55e898cf98f2867fc65898990e5da78ec
Wei Wang <ww000721@gmail.com>
2025-05-15 09:53:17 -0400

    Update clipboard.lua

--- nvim/lua/plugins/clipboard.lua
+++ nvim/lua/plugins/clipboard.lua
@@ -8,7 +8,7 @@ return {
         vim.api.nvim_create_autocmd("TextYankPost", {
             callback = function()
                 if vim.v.event.operator == "y" and vim.v.event.regname == "" then
-                osc52.copy_register('"')
+                    osc52.copy_register('"')
                 end
             end,
         })
