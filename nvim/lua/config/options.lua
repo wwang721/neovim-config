35f01d14b763ed7cd55f98478ea7dc9b36ab6532
Wei Wang <ww000721@gmail.com>
2025-05-22 18:49:26 -0400

    Update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -28,10 +28,10 @@ opt.cursorline = true -- Enable highlighting of the current line
 -- opt.guicursor = "" -- disable cursor shape changes
 -- use Neovim's default cursor shapes, see :h 'guicursor'
 opt.guicursor = {
-    -- "n-v-c:block",
-    -- "i-ci-ve:ver25",
-    -- "r-cr:hor20",
-    -- "o:hor50",
+    "n-v-c:block",
+    "i-ci-ve:ver25",
+    "r-cr:hor20",
+    "o:hor50",
     "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
     "sm:block-blinkwait175-blinkoff150-blinkon175"
 }
