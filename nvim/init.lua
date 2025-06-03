c59c5dcf7aa6cbd6907ab54186a7ab95f35318e7
Wei Wang <ww000721@gmail.com>
2025-06-03 17:56:02 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,11 @@
 -- Set options (must be first)
 require("config.options")
 
--- Key mappings
--- require("config.keymaps")
-
 -- Autocommands
 require("config.autocmds")
 
 -- Plugin manager and plugin specs
 require("config.lazy")
+
+-- Key mappings
+-- require("config.keymaps")
